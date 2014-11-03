574ab645e96f051a5482871c96418e1e87770854
Mike Swartz <mswartz@gmail.com>
2014-11-02 22:22:26 -0500

    got posts submitting, notes added and other good stuff

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -2,17 +2,44 @@ Template.postSubmit.events({
   'change form': function(e){
   	e.preventDefault();
 
+    //get all the embedly stuff
     var data = Embedly.extract($('#url').val());
+
+    //check for dupes
+    var postWithSameLink = Posts.findOne({url: data.url});
+    if (postWithSameLink) {
+      $('#error-dupe').show();
+    } else {
+      $('#error-dupe').hide();
+    }
+
+    //just grab the first thumbnail
     var thumbnail = data.images[0].url;
     data.thumbnail = thumbnail;
 
+    //cache this crunched data in a session var
     Session.set('post_cache', data);
+  },
+  'submit form': function(){
+      var note = $('#note').val();
+      var post = Session.get('post_cache');
+      post.note = note;
+
+      Meteor.call('postInsert', post, function(error, result) {
+          // display the error to the user and abort
+          if (error){
+            return alert(error.reason);
+          }
+
+          // show this result but route anyway
+          if (result.postExists){
+            alert('This link has already been posted');
+          }
+      });
+      Router.go('postsList');
+    }
   }
-
-  //todo: parse link info with embedly
-
-  //todo: method to insert into db
-});
+);
 
 Template.postSubmit.helpers({
   'post_preview': function(){
